546b27d17e663a6435851ad9d504a72a29ab1133
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-02-23 03:57:29 -0800

    Printing the number of days remaining

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -1,13 +1,3 @@
-#include "main.h"
-
-/**
-* convert_day - converts day of month to day of year, without accounting
-* for leap year
-* @month: month in number format
-* @day: day of month
-* Return: day of year
-*/
-
 int convert_day(int month, int day)
 {
     switch (month)
@@ -50,4 +40,3 @@ int convert_day(int month, int day)
     }
     return (day);
 }
-
