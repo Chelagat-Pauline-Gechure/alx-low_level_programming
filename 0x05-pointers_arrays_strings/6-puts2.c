e9b08d0e4e98e666d394f4d8ae4f41496da9c685
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-02-27 13:37:45 -0800

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - Print string characters from first character,
+ *followed by a new line.
+ *
+ * @s: This pointer point to a char or string
+ */
+void puts2(char *s)
+{
+int a;
+for (a = 0; s[a] != '\0'; a++)
+{
+if (a % 2 == 0)
+{
+_putchar(s[a]);
+}
+}
+_putchar('\n');
+}
