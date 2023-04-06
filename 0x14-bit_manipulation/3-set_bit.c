5b9fcf5b96629539c1d4e792b0ef02c506af8418
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-04-06 11:03:57 -0700

    Write a function that sets the value of a bit to 1 at a given index.

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,10 +9,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+    unsigned long int mask;
     if (index >= sizeof(unsigned long int) * 8)
         return (-1);
 
-    unsigned long int mask;
     mask = 1UL << index;
     *n |= mask;
 
