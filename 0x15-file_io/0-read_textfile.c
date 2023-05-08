fe1299ad1e244021d74243550c59ab7d071f0dba
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-05-08 14:40:29 -0700

    Write a function that reads a text file and prints it to the POSIX standard output.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/**
+ * read_textfile - reads a text file and prints it to POSIX standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ *
+ * Return: number of letters actually read and printed, or 0 on error
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char buffer[1024];
+	ssize_t lenr, lenw;
+	ssize_t total = 0; /* Use ssize_t for signed integer comparison */
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	while ((lenr = read(fd, buffer, sizeof(buffer))) > 0)
+	{
+		/* Cast letters to ssize_t for comparison */
+		if (letters > 0 && total + lenr > (ssize_t)letters)
+		/* Cast letters to ssize_t for arithmetic */
+			lenr = (ssize_t)letters - total;
+		lenw = write(STDOUT_FILENO, buffer, lenr);
+		if (lenw != lenr)
+		{
+			close(fd);
+			return (0);
+		}
+		total += lenw;
+		/* Cast letters to ssize_t for comparison */
+		if (letters > 0 && total >= (ssize_t)letters)
+			break;
+	}
+
+	close(fd);
+	return (total);
+}
