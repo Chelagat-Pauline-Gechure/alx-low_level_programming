0287ba221f336be544565bd7bfa0c1436e4fc3a6
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-10-12 00:50:15 +0300

    Chnaged user permissions

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,7 +5,7 @@
  * @array: Integer array to be searched
  * @size: size of the array
  * @value: value to search for
- * Return: Always EXIT_SUCCESS
+ * Return: Always EXIT_SUCCESS.
  */
 
 int linear_search(int *array, size_t size, int value)
